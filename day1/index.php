6269de44c5bfe4afc97997f5d0bb501c6c4e0df7
WD Ronju <github.ronju@gmail.com>
2023-11-13 17:44:26 +0600

    day 1 and day2 done

---
+++ day1/index.php
@@ -0,0 +1,15 @@
+<?php
+
+$name = "Ronju islam";
+$age = 30;
+
+$workingHrTotal = 50;
+$HrlyRate = 20;
+
+$totalAmount = $workingHrTotal * $HrlyRate;
+
+echo "
+$name is  $age Years Old.<br> 
+His Hourly Rate is $HrlyRate. <br> 
+He Worked $workingHrTotal Hour in this month.<br>
+his total of working hour in this month is $ $totalAmount";
---
+++ day2/index.php
@@ -0,0 +1,79 @@
+<?php
+
+// //if statment
+// $number = 401;
+
+// if($number % 2 == 0){
+//  echo  "$number is Even Number";
+// }else{
+//     echo "$number is odd Number";
+// };
+
+
+ // Lets Make  a result Grading system 
+//  $BanglaMarks = 132220;
+//  $EnglishMarks = 0;
+//  $MathMarks = 8;
+
+//  $totalMarks = $BanglaMarks + $EnglishMarks + $MathMarks;
+//  $AVG_Marks = $totalMarks  / 3;
+
+
+// if($AVG_Marks >= 80 && $AVG_Marks <= 100){
+//     echo  "Congrats !! Your Avarage Marks is ($AVG_Marks) You got A+";
+// }
+// elseif($AVG_Marks >= 60 && $AVG_Marks <= 79){
+//     echo  "Congrats !! Your Avarage Marks is ($AVG_Marks) and You got A";
+// }
+// elseif($AVG_Marks >= 40 && $AVG_Marks <= 59){
+//     echo  "Congrats !! Your Avarage Marks is ($AVG_Marks) and You got B";
+// }
+// elseif($AVG_Marks >= 33 && $AVG_Marks <= 39){
+//     echo  "Congrats !! Your Avarage Marks is ($AVG_Marks) and You got C";
+// }
+// elseif($AVG_Marks >= 0 && $AVG_Marks <= 32){
+//     echo  "Sorry !! Your Avarage Marks is ($AVG_Marks) and You Are Faillllll...";
+// }
+// else{
+//     echo "Opps.. invalid Number.";
+// }
+
+
+// Lets make a project that shows me Day name 
+
+// $weekDay =  56;
+
+// switch($weekDay){
+//     case 1: echo "Today is Saturday.";
+//     break;
+
+//     case 2: echo "Today is Sunday.";
+//     break;
+
+//     case 3: echo "Today is Monday.";
+//     break;
+
+//     case 4: echo "Today is Tuesday.";
+//     break;
+
+//     case 5: echo "Today is Wednesday.";
+//     break;
+
+//     case 6: echo "Today is Thursday.";
+//     break;
+
+//     case 7: echo "Today is Friday.";
+//     break;
+
+//     default: echo "Invalid  Day Number.";
+//     break;
+// };
+
+
+// Lets practice For Loop
+
+for($i = 0; $i <= 10; $i++){
+    echo $i. "<br>";
+}
+
+
